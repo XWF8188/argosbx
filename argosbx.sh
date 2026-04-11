920595e2b0dab69153563e4b4cef4b3890dea148
甬哥侃侃侃ygkkk <121604513+yonggekkk@users.noreply.github.com>
2026-04-11 22:18:58 +0800

    Update argosbx.sh

--- argosbx.sh
+++ argosbx.sh
@@ -1510,10 +1510,10 @@ fi
 if grep hy2-sb "$HOME/agsbx/sb.json" >/dev/null 2>&1; then
 echo "💣【 Hysteria2 】节点信息如下："
 port_hy2=$(cat "$HOME/agsbx/port_hy2")
-hy2_ports=$(iptables -t nat -nL --line 2>/dev/null | grep -w "$port_hy2" | awk '{print $8}' | sed 's/dpts*://')
+hy2_ports=$(iptables -t nat -nL --line 2>/dev/null | grep -w "$port_hy2" | awk '{print $8}' | sed 's/dpts://; s/dpt://' | tr '\n' ',' | sed 's/,$//')
 if [ -n "$hy2_ports" ]; then
 echo "Hysteria2跳跃端口已开启：$hy2_ports"
-cmhy2pt=$(echo $hy2_ports | tr ' :' '-,' | tr -s ',')
+cmhy2pt=$(echo $hy2_ports | sed 'y/: /-,/')
 hyps=$cmhy2pt
 sbhy2pt=$(echo "$hy2_ports" | grep -o '[0-9]\+:[0-9]\+' | sed 's/.*/"&"/' | paste -sd,)
 sbhy2ports(){
