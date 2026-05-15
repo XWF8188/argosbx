8be39a9367d3f434bde9a80ec1fc1a474ace7fee
甬哥侃侃侃ygkkk <121604513+yonggekkk@users.noreply.github.com>
2026-05-16 07:44:05 +0800

    Update argosbx.sh

--- argosbx.sh
+++ argosbx.sh
@@ -153,7 +153,7 @@ elif [ "$v4_ok" = true ] && [ "$v6_ok" != true ]; then
 case "$warp" in *s4*|x) sbyx='ipv4_only' ;; *) sbyx='prefer_ipv6' ;; esac
 case "$warp" in *x4*) xryx='ForceIPv4' ;; *x*) xryx='ForceIPv6v4' ;; *) xryx='ForceIPv4v6' ;; esac
 elif [ "$v4_ok" != true ] && [ "$v6_ok" = true ]; then
-case "$warp" in *s6*) sbyx='ipv6_only' ;; *) sbyx='prefer_ipv4' ;; esac
+case "$warp" in *s6*|x) sbyx='ipv6_only' ;; *) sbyx='prefer_ipv4' ;; esac
 case "$warp" in *x6*) xryx='ForceIPv6' ;; *x*) xryx='ForceIPv4v6' ;; *) xryx='ForceIPv6v4' ;; esac
 fi
 }
