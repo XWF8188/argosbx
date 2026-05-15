4de5a41391087f19d5e546daed9e2638ad5cd691
甬哥侃侃侃ygkkk <121604513+yonggekkk@users.noreply.github.com>
2026-05-15 16:39:05 +0800

    Update argosbx.sh

--- argosbx.sh
+++ argosbx.sh
@@ -2287,10 +2287,12 @@ iptables -t nat -A PREROUTING -p udp --dport "$port" -j DNAT --to-destination :$
 ip6tables -t nat -A PREROUTING -p udp --dport "$port" -j DNAT --to-destination :$hyport
 done
 netfilter-persistent save >/dev/null 2>&1
-rc-update show default | grep -q 'iptables'  || rc-update add iptables  >/dev/null 2>&1
-rc-update show default | grep -q 'ip6tables' || rc-update add ip6tables >/dev/null 2>&1
+if command -v rc-service >/dev/null 2>&1; then
+rc-update show default 2>/dev/null | grep -q 'iptables' || rc-update add iptables >/dev/null 2>&1
+rc-update show default 2>/dev/null | grep -q 'ip6tables' || rc-update add ip6tables >/dev/null 2>&1
 rc-service iptables save >/dev/null 2>&1
 rc-service ip6tables save >/dev/null 2>&1
+fi
 fi
 cip
 echo
